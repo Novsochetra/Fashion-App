327cfda5a2328a7e0e5972988875b8822782924c
sochetra NOV <sochetra12.nov@gmail.com>
2020-08-06 20:09:49 +0700

    add testing share element

--- src/HomeScreen.tsx
+++ src/HomeScreen.tsx
@@ -1,19 +1,121 @@
-import React, { useRef, LegacyRef, MutableRefObject } from 'react'
-import { View, Text, StyleSheet, TouchableOpacity } from 'react-native'
+import React, {
+  useRef,
+  createRef,
+  MutableRefObject,
+  ReactElement,
+  useEffect,
+  useState,
+} from 'react'
+import {
+  View,
+  Text,
+  StyleSheet,
+  TouchableOpacity,
+  Animated,
+  Easing,
+  Dimensions,
+} from 'react-native'
+const { width: WINDOW_WIDTH, height: WINDOW_HEIGHT } = Dimensions.get('window')
+const BOX_WIDTH = 100
+const BOX_HEIGHT = 100
+const MIDDLE_POS = (WINDOW_HEIGHT - BOX_WIDTH) / 2
 
-export const HomeScreen = () => {
-  let boxRef: { [key: number]: MutableRefObject<TouchableOpacity> } = {}
-  const openBox = () => {}
-  const closeBox = () => {}
+const DATA = [...Array(5).keys()]
 
-  const createBoxRef = (i): void => {
-    boxRef[i] = useRef()
+export const HomeScreen = (): ReactElement => {
+  const [boxPosition, setBoxPosition] = useState<{ [key: number]: string }>({
+    1: 'relative',
+    2: 'relative',
+    3: 'relative',
+    4: 'relative',
+    5: 'relative',
+  })
+  const boxes: {
+    [key: number]: { width: Animated.Value; translateY: Animated.Value; height: Animated.Value }
+  } = {}
+
+  const onOpenBox = (index: number): void => {
+    const currentBox = boxes[index]
+
+    Animated.parallel([
+      Animated.timing(currentBox.width, {
+        toValue: WINDOW_WIDTH,
+        duration: 200,
+        easing: Easing.quad,
+        useNativeDriver: false,
+      }),
+
+      Animated.timing(currentBox.height, {
+        toValue: WINDOW_HEIGHT,
+        duration: 200,
+        easing: Easing.quad,
+        useNativeDriver: false,
+      }),
+
+      Animated.timing(currentBox.translateY, {
+        toValue: 0,
+        duration: 200,
+        easing: Easing.quad,
+        useNativeDriver: false,
+      }),
+    ]).start(() => setBoxPosition((prev) => ({ ...prev, [index]: 'absolute' })))
+  }
+
+  const onCloseBox = (index: number): void => {
+    const currentBox = boxes[index]
+    // setBoxPosition((prev) => ({ ...prev, [index]: 'relative' }))
+    Animated.parallel([
+      Animated.timing(currentBox.width, {
+        toValue: BOX_WIDTH,
+        duration: 200,
+        easing: Easing.quad,
+        useNativeDriver: false,
+      }),
+
+      Animated.timing(currentBox.height, {
+        toValue: BOX_HEIGHT,
+        duration: 200,
+        easing: Easing.quad,
+        useNativeDriver: false,
+      }),
+
+      Animated.timing(currentBox.translateY, {
+        toValue: 0,
+        duration: 200,
+        easing: Easing.quad,
+        useNativeDriver: false,
+      }),
+    ]).start(() => setBoxPosition((prev) => ({ ...prev, [index]: 'relative' })))
   }
 
   return (
     <View style={styles.container}>
       {[...Array(5).keys()].map((v, i) => {
-        return <TouchableOpacity style={styles.boxContainer} />
+        boxes[i + 1] = {
+          width: new Animated.Value(BOX_WIDTH),
+          height: new Animated.Value(BOX_HEIGHT),
+          translateY: new Animated.Value(0),
+        }
+        return (
+          <TouchableOpacity key={`box-${i}`} onPress={() => onOpenBox(i + 1)}>
+            <Animated.View
+              style={[
+                styles.boxContainer,
+                {
+                  width: boxes[i + 1].width,
+                  height: boxes[i + 1].height,
+                  transform: [{ translateY: boxes[i + 1].translateY }],
+                  position: boxPosition[i + 1],
+                },
+              ]}
+            >
+              <Text>{i + 1}</Text>
+              <TouchableOpacity onPress={() => onCloseBox(i + 1)}>
+                <Text>Close</Text>
+              </TouchableOpacity>
+            </Animated.View>
+          </TouchableOpacity>
+        )
       })}
     </View>
   )
@@ -22,14 +124,15 @@ export const HomeScreen = () => {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
+    // justifyContent: 'center',
+    // alignItems: 'center',
   },
 
   boxContainer: {
+    justifyContent: 'center',
     backgroundColor: 'red',
-    margin: 10,
-    width: 100,
-    height: 100,
+    width: BOX_WIDTH,
+    height: BOX_HEIGHT,
+    marginBottom: 10,
   },
 })
